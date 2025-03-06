ba54422cf8d6ce183ca21c0af40e6eb37cff72f9
Brian <brian.kiprono8@student.moringaschool.com>
2025-03-06 22:28:40 +0300

    practicing arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+ const num1= 31;
+ const num2 = 2;
+ const multiply= num1*num2; 
+
+//making a random number greater than 0
+const random = Math.floor(Math.random()*100) +1;
+
+//mod AKA remainder operator
+let num4 =14;
+let num3= 5
+const mod = num4%num3;
+
+// returning the highest number in a given set
+let num5 = 12;
+let num6 = 10;
+let num8 = 20;
+const max =Math.max(num5,num6,num8);
+
